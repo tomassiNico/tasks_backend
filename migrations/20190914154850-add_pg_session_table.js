2c56b2a5dd29179a21ea433ef7080bbd0dcfd22b
Nicolás Tomassi <xtomassix@gmail.com>
2019-09-14 12:57:38 -0300

    nueva migracion para las sesiones en heroku

---
+++ migrations/20190914154850-add_pg_session_table.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('session',{
+      sid: {
+        type: Sequelize.STRING,
+        primaryKey: true
+      },
+      sess: {
+        type: Sequelize.JSON
+      },
+      expire: {
+        type: 'TIMESTAMP',
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('session');
+};
