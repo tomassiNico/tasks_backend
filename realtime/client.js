9fb7a00d2d475b8532ccb470fbca6eb4a51602d3
Nicolás Tomassi <xtomassix@gmail.com>
2019-09-14 13:03:42 -0300

    cambiamos el host del websocket para produccion

--- realtime/client.js
+++ realtime/client.js
@@ -1,6 +1,11 @@
 const io = require('socket.io-client');
+let host = 'http://localhost:3000';
 
-let socket = io.connect('http://localhost:3000', {reconect:true});
+if(process.env.NODE_ENV && process.env.NODE_ENV == 'production'){
+  host = 'https://peaceful-mountain-37777.herokuapp.com/';
+}
+
+let socket = io.connect(host, {reconect:true});
 
 socket.on('connect',function(){
   console.log("\n\nConectado con server NodeJS\n\n")
